9f96515cccc47c9751e5ae59c4d4c349b75b7d5c
Kuirak <newdrako@gmx.net>
2013-05-14 00:09:47 +0200

    More research on how sequelize works - now  findorCreate is used

--- Node/app.js
+++ Node/app.js
@@ -33,19 +33,30 @@ app.get('/users', user.list);
 
  app.get('/init',function(req,res){
      var Category = app.get('models').Category;
-     var atom_category = Category.create({title:'Atomkraft',id:1}).success(function(category){
+     var atom_category = Category.findOrCreate({title:'Atomkraft'}).success(function(category){
          console.log("Success on creating %s category.",category.title)
          atom_category=category;
      });
-     var Article = app.get('models').Article;
-     var first_article = Article.create({title:"Atom ist nuklear",id:3,date:new Date(),article:"Bla fastesd adses atom blubb dsdasd"}).success(function(article){
-         console.log("Success on creating %s article", article.title);
+     var umwelt_category = Category.findOrCreate({title:'Umwelt'}).success(function(category){
+         console.log("Success on creating %s category.",category.title)
+         umwelt_category=category;
+         var Article = app.get('models').Article;
+         Article.findOrCreate({id:1},{title: "Atom ist nuklear", date: new Date(88,8,8), article: "Bla fastesd adses atom blubb dsdasd"}).success(function (article) {
+             console.log("Success on creating %s article", article.title);
+             article.hasCategory(atom_category).success(function(result){
+                 if(!result){
+                     article.addCategory(atom_category).success(function () {     });
+                 }
+             });
+             article.hasCategory(umwelt_category).success(function(result){
+                 if(!result){
+                     article.addCategory(umwelt_category).success(function () {     });
+                 }
+             });
 
-             article.addCategory(atom_category).success(function(){
+         });
+     });
 
-
-         })
-     }) ;
 
      res.write("Initialized!");
      res.end();
@@ -55,11 +66,18 @@ app.get("/first",function(req,res){
 
 
     var Article = app.get('models').Article;
-    Article.find(3).success(function(art){
+    Article.find(1).success(function(art){
         var title = art.title;
         var article = art.article;
         var date = art.date;
-        res.render("article",{title:title,article:article,date:date});
+        art.getCategories().success(function(cat) {
+            var categories=[];
+            for (var i = 0; i < cat.length; i++) {
+                categories.push( cat[i].title);
+            }
+            res.render("article",{title:title,article:article,categories:categories,date:date});
+        } )
+
     });
 
 });
