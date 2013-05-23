c03df2522cdab4de07677262f99e5e72f121358a
Kuirak <newdrako@gmx.net>
2013-05-23 12:11:35 +0200

    Added #9 simple login
    login: zeitkurier
    password: kurier-der-zeit

--- Node/app.js
+++ Node/app.js
@@ -18,12 +18,14 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('models',require('./models'));
+app.use(express.basicAuth('zeitkurier','kurier-der-zeit'));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
