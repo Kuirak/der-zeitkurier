bc9678fd8aff638fb841d6be0f83cd77e584aa72
Kuirak <newdrako@gmx.net>
2013-06-25 18:58:29 +0200

    Started key mapping

--- GPIO/zeitmain.py
+++ GPIO/zeitmain.py
@@ -6,10 +6,11 @@ import os
 
 os.system("fuser -k /dev/video0")
 scanner = zeitscanner.Scanner('/dev/video0')
+typewriter =zeittypewriter.Typewriter()
 
 while True:
     dbClient = zeitarticledbclient.ArticleDatabaseClient("localhost:3000")
-    typewriter =zeittypewriter.Typewriter()
+
     result = scanner.scan_one()
     article = dbClient.get_article(1)
     if not article:
--- GPIO/zeittypewriter.py
+++ GPIO/zeittypewriter.py
@@ -1,9 +1,18 @@
 from __future__ import print_function
 import time
 import random
+import csv
 
 
 class Typewriter:
+    def __init__(self):
+        f = open('mapping.csv', 'rb')
+        reader = csv.DictReader(f, delimiter=",")
+        f.close()
+        self.keymap = {}
+        for row in reader:
+            self.keymap[row["key"]] = {"output": row["output"], "capslock": row["capslock"] == 'TRUE'}
+
     def printArticle(self, article):
         for line in article.title:
             self.printLine(line)
